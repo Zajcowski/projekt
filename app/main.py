81e1ed3007c33ca06631e4ffd388b323fff06ed4
Jan Rojek <janekrojek683@gmail.com>
2024-01-11 12:58:02 +0100

    db connect

--- app/main.py
+++ app/main.py
@@ -1,8 +1,35 @@
-# uvicorn main:app --reload
+import json
 from fastapi import FastAPI
+import pymysql
+from dotenv import load_dotenv
+import os
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"test"}
+load_dotenv()
+
+MYSQL_HOST = os.getenv("MYSQL_HOST")
+MYSQL_PORT = os.environ.get("MYSQL_PORT", 3306)
+MYSQL_USER = os.environ.get("MYSQL_USER", 'root')
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+MYSQL_DB = os.environ.get("MYSQL_DB", 'mysql')
+
+def connect():
+    return pymysql.connect(
+        host=MYSQL_HOST,
+        port=MYSQL_PORT,
+        user=MYSQL_USER,
+        password=MYSQL_PASSWORD,
+        database=MYSQL_DB
+    )
+
+@app.get("/name")
+async def get_name():
+    conn = connect()
+    cursor = conn.cursor()
+    query = "SELECT `User` From `user`"
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    print(json.dumps(rows))
+    conn.close()
+    return {"table_data": json.dumps(rows)}
