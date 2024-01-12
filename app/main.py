fd47b2c0c07ba35df97d0782372ca8177c075bbf
Jan Rojek <janekrojek683@gmail.com>
2024-01-12 12:43:41 +0100

    do poprawy

--- app/main.py
+++ app/main.py
@@ -1,18 +1,20 @@
 import json
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 import pymysql
 from dotenv import load_dotenv
 import os
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 load_dotenv()
-
+'''
 MYSQL_HOST = os.getenv("MYSQL_HOST")
 MYSQL_PORT = os.environ.get("MYSQL_PORT", 3306)
 MYSQL_USER = os.environ.get("MYSQL_USER", 'root')
 MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
-MYSQL_DB = os.environ.get("MYSQL_DB", 'mysql')
+MYSQL_DB = os.environ.get("MYSQL_DB", 'samochody')
 
 def connect():
     return pymysql.connect(
@@ -22,7 +24,14 @@ def connect():
         password=MYSQL_PASSWORD,
         database=MYSQL_DB
     )
-
+'''
+db_config = {
+    'host' : 'my-db',
+    'user' : 'root',
+    'password' : 'mypass123',
+    'database' : 'samochody'
+}
+'''
 @app.get("/name")
 async def get_name():
     conn = connect()
@@ -33,3 +42,23 @@ async def get_name():
     print(json.dumps(rows))
     conn.close()
     return {"table_data": json.dumps(rows)}
+'''
+
+
+def get_all_samochody():
+    try:
+        connection = pymysql.connect(**db_config)
+        cursor = connection.cursor()
+        query = "SELECT * FROM marki"
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        return rows
+    finally:
+        cursor.close()
+        connection.close()
+
+
+@app.get("/samochody")
+def get_all_samochody_endpoint(request, Request):
+    all_samochody = get_all_samochody()
+    return templates.TemplateResponse("all_samochody.html", {"request": request, "data": all_samochody})
